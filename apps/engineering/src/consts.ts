2ee7e4196f2b3714c571388e5d9618fd4c542b17
litmus-zhang <abdulsalamlukmon9@gmail.com>
2025-10-14 20:01:45 +0100

    updated invite link

--- apps/engineering/src/consts.ts
+++ apps/engineering/src/consts.ts
@@ -8,6 +8,6 @@
  */
 export const SITE_NAME = 'Neophyte';
 export const Logo_URL = "https://res.cloudinary.com/dk0pompea/image/upload/v1760464268/neophyte-systems/logo/primary.svg";
-export const MeetingUrl = "https://zcal.co/dynage/15min";
+export const MeetingUrl = "https://zcal.co/neophyte/30min";
 
 
